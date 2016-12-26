fe877ce0a0223343bf97f1eb0753d2ec9ea0eea3
yoyonel <yoyonel@hotmail.com>
2016-12-26 19:48:41 +0100

    Fix sur le current dir.

--- run.sh
+++ run.sh
@@ -16,6 +16,8 @@ set -e
 # url: https://gist.github.com/d11wtq/8699521
 # docker run --volume $SSH_AUTH_SOCK:/ssh-agent --env SSH_AUTH_SOCK=/ssh-agent ubuntu ssh-add -l
 
+# url: http://serverfault.com/questions/634883/why-cant-i-cd-to-a-directory-with-docker-run
+
 # Run the container with shared X11
 docker run\
   --net=host\
@@ -26,4 +28,5 @@ docker run\
   -v "/tmp/.X11-unix:/tmp/.X11-unix:rw"\
   -v $SSH_AUTH_SOCK:/ssh-agent \
   -e SSH_AUTH_SOCK=/ssh-agent \
+  -w $PWD \
   -it $1 $SHELL
